3e04141e689162b67a1927978194fd5b3da84721
Xavier QUIÈVRE <xavier.quievre@webdealauto.com>
2019-01-22 16:57:48 +0100

    [UPDATE] Client side

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { AppComponent } from './app.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { DocumentComponent } from './document/document.component';
+//import { DocumentService } from './document.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DocumentListComponent,
+    DocumentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
